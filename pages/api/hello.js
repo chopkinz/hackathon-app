b824eb1d752a3147db7e0f0c946d61f86fd6aff8
Chase <chasehop334@gmail.com>
2022-02-12 16:49:43 -0600

    initial commit

---
+++ pages/api/hello.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const options = {
+  method: "POST",
+  url: "https://socialanimal.p.rapidapi.com/api/v1/search",
+  headers: {
+    "x-rapidapi-host": "socialanimal.p.rapidapi.com",
+    "x-rapidapi-key": "14d28dc29emshea1c4806b859341p165647jsn1f7d23569338",
+  },
+};
+
+axios
+  .request(options)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
