e138881fdb64bd2d995a272dc463c5c29e776579
Carlos Domani <carlos.a.domani@gmail.com>
2022-02-13 03:08:02 -0600

    Showcase

---
+++ components/Main/ShowcaseContainer.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+
+
+export const ShowcaseContainer = () => {
+  return(
+    <div className="w-[800px] h-[800px]">
+      <div className="grid grid-cols-5">
+
+      </div>
+    </div>
+  )
+}
---
+++ pages/showcase.tsx
@@ -0,0 +1,10 @@
+import { ShowcaseContainer } from "../components/Main/ShowcaseContainer";
+
+
+const Wordle = () => {
+  return(
+    <ShowcaseContainer />
+  )
+}
+
+export default Wordle;
